d193e958d35f7f8523fbbfdb7d298c9b01b33716
Paulina Ciesińska <paulinaciesinska@gmail.com>
2023-04-18 18:13:30 +0200

    Remove class active in js

--- js/script.js
+++ js/script.js
@@ -6,9 +6,20 @@ function titleClickHandler(event){
 
   /* remove class 'active' from all article links  */
 
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
+
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.posts .active');
+
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
 
   /* get 'href' attribute from the clicked link */
 
@@ -22,3 +33,4 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
