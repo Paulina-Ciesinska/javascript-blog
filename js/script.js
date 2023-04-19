2eeb0c1224c2aa05b2c59b729a1241b2e3f4c2c6
Paulina Ciesińska <paulinaciesinska@gmail.com>
2023-04-19 21:57:17 +0200

    Finish 1# part - section all posts

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
   function titleClickHandler(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
 
@@ -12,18 +13,10 @@
     activeLink.classList.remove('active');
   }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
 
   console.log('clickedElement:', clickedElement);
   clickedElement.classList.add('active')
-
-  /* [DONE] remove class 'active' from all articles */
-
-  for(let activeLink of activeLinks){
-    activeLink.classList.remove('active');
-  }
-
-  /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts .active');
@@ -34,9 +27,19 @@
 
   /* get 'href' attribute from the clicked link */
 
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
+
   /* find the correct article using the selector (value of 'href' attribute) */
 
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle)
+
   /* add class 'active' to the correct article */
+
+  console.log('clickedElement:', targetArticle);
+  targetArticle.classList.add('active')
+
 }
 
 const links = document.querySelectorAll('.titles a');
