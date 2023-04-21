32c1c80e3e82d01be6f29c43155c51db473df4a7
Paulina Ciesińska <paulinaciesinska@gmail.com>
2023-04-21 20:13:16 +0200

    Add part 2 (not whole)

--- js/script.js
+++ js/script.js
@@ -42,9 +42,64 @@
 
 }
 
+
+//GENERATE TITLE LIST 
+
+  const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles',
+    optArticleTagsSelector = '.post-tags .list',
+    optArticleAuthorSelector = '.post-author',
+    optTagsListSelector = '.list.tags';
+
+function generateTitleLinks(customSelector = ''){
+  console.log(customSelector = '', customSelector = ''); /*skąd się wziął customSelector??*/
+
+  /* [DONE] remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+
+    /* for each article */
+
+    let html = '';
+
+    const articles = document.querySelectorAll(optArticleSelector);
+
+    for(let article of articles){
+      
+    /* get the article id */
+
+    const articleId = article.getAttribute('id');
+    console.log(articleId);
+
+    /* find the title element */
+    /* get the title from the title element */
+
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* create HTML of the link */
+
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(linkHTML);
+
+    /* insert link into titleList */
+
+    titleList.insertAdjacentHTML('beforeend', linkHTML);
+
+    /* insert link into html variable */
+      html = html + linkHTML;
+
+      console.log(html);
+    }
+
+    titleList.innerHTML = html;
+
+}
+
+generateTitleLinks();
+
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
-
