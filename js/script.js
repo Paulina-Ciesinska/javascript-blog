f3dee6c12f3a2d9bb25c0dcf762e86a38f816937
Paulina Ciesińska <paulinaciesinska@gmail.com>
2023-04-25 18:03:19 +0200

    Add third part - Generate tags

--- js/script.js
+++ js/script.js
@@ -46,10 +46,10 @@ function titleClickHandler(event){
 
 //GENERATE TITLE LIST 
 
-const optArticleSelector = '.post',
-  optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles',
-  optTagsListSelector = '.list.tags';
+const optArticleSelector = '.post';
+const optTitleSelector = '.post-title';
+const optTitleListSelector = '.titles';
+const optArticleTagsSelector = '.post-tags .list';
 
 function generateTitleLinks(){
   console.log();
@@ -110,52 +110,105 @@ function generateTags(){
 
   /* find all articles */
 
-
+  const articles = document.querySelectorAll(optArticleSelector);
+  console.log(articles);
 
   /* START LOOP: for every article: */
 
-
+  for(let article of articles){
 
     /* find tags wrapper */
 
-
+    const tagsWrapper = article.querySelector(optArticleTagsSelector);
+    console.log(tagsWrapper);  
+    /*nie skasowałam problematycznej zmiennej, a mniej więcej w tym momencie zniknęła mi znowu lewa kolumna strony*/
 
     /* make html variable with empty string */
 
-
+    let html = '';
 
     /* get tags from data-tags attribute */
 
-
+    const articleTags = article.getAttribute('data-tags');
+    console.log(articleTags);
 
     /* split tags into array */
 
-
+    const articleTagsArray = articleTags.split(' ');
+    console.log(articleTagsArray);
 
     /* START LOOP: for each tag */
 
-
+    for(let tag of articleTagsArray){
+      console.log(tag);
 
       /* generate HTML of the link */
 
-
+      const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>';
+      console.log(linkHTML);
 
       /* add generated code to html variable */
 
+      html = html + linkHTML;
+      console.log(html);
 
+      /* END LOOP: for each tag */
 
-    /* END LOOP: for each tag */
-
-
+    }
 
     /* insert HTML of all the links into the tags wrapper */
 
+    tagsWrapper.insertAdjacentHTML('beforeend', linkHTML);
 
 
-  /* END LOOP: for every article: */
+    /* END LOOP: for every article: */
 
+  }
 
-  
 }
 
 generateTags();
+
+function tagClickHandler(event){
+  /* prevent default action for this event */
+
+
+
+  /* make new constant named "clickedElement" and give it the value of "this" */
+
+  
+
+  /* make a new constant "href" and read the attribute "href" of the clicked element */
+
+  /* make a new constant "tag" and extract tag from the "href" constant */
+
+  /* find all tag links with class active */
+
+  /* START LOOP: for each active tag link */
+
+    /* remove class active */
+
+  /* END LOOP: for each active tag link */
+
+  /* find all tag links with "href" attribute equal to the "href" constant */
+
+  /* START LOOP: for each found tag link */
+
+    /* add class active */
+
+  /* END LOOP: for each found tag link */
+
+  /* execute function "generateTitleLinks" with article selector as argument */
+}
+
+function addClickListenersToTags(){
+  /* find all links to tags */
+
+  /* START LOOP: for each link */
+
+    /* add tagClickHandler as event listener for that link */
+
+  /* END LOOP: for each link */
+}
+
+addClickListenersToTags();
